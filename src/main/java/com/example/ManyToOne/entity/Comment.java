c135b79e6104e85287da28ec26f825a6b33f6fe8
DivyakanthReddy123 <44220908+DivyakanthReddy123@users.noreply.github.com>
2023-03-21 12:54:57 +0530

    added comments for Bi Directional OneToMany

--- src/main/java/com/example/ManyToOne/entity/Comment.java
+++ src/main/java/com/example/ManyToOne/entity/Comment.java
@@ -21,8 +21,12 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Lob // when string exceeds the max size ( 65k  bytes ) , then this annotation will be helpful to map Large Objects
+    @Lob // ( here it is unnecessary ) when string exceeds the max size ( 65k  bytes ) , then this annotation will be helpful to map Large Objects
     private String content;
+
+    // it is better to use @ManyToOne over @OneToMany for Unidirectional to save storage and other reasons .
+
+    // the default fetch type of @ManyToOne is Eager , since this is bad for performance if we have more child entities , therefore we use Lazy .
     @ManyToOne(fetch = FetchType.LAZY, optional = false) // optional= false indicates that each Comment instance belongs to exactly one Tutorial instance .
     @JoinColumn(name = "tutorial_id", nullable = false) // name of the foreign key column and its value cannot be null
     @OnDelete(action = OnDeleteAction.CASCADE) // when a tutorial is deleted then the corresponding comments of that tutorialId will be deleted
